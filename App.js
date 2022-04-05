5e1ace5955b5dc48f4cd9a3c24dcafa0c01afdf5
Darian Jason <darianjason@gmail.com>
2022-04-05 12:04:53 +0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {StyleSheet, SafeAreaView, Text} from 'react-native';
+
+const App: () => Node = () => {
+  return (
+    <SafeAreaView style={styles.screen}>
+      <Text>Hello World</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
