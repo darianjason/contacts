04c43df086681b0148c49b58f5c723f72a470693
Darian Jason <darianjason@gmail.com>
2022-04-06 15:10:05 +0700

    Add basic screens

---
+++ screens/Contacts/Contacts.js
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+const Contacts = ({navigation}) => {
+  return (
+    <View>
+      <Text>Contacts Screen</Text>
+    </View>
+  );
+};
+
+export default Contacts;
---
+++ screens/index.js
@@ -0,0 +1,3 @@
+export {default as Contacts} from './Contacts/Contacts';
+export {default as ContactDetails} from './ContactDetails/ContactDetails';
+export {default as EditContact} from './EditContact/EditContact';
