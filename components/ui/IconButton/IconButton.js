b038c97671fb33ce9b54601b9b9d6a1105b6dc28
Darian Jason <darianjason@gmail.com>
2022-04-12 13:52:35 +0700

    Make icon button component more flexible

--- components/ui/IconButton/IconButton.js
+++ components/ui/IconButton/IconButton.js
@@ -4,16 +4,45 @@ import {Pressable} from 'react-native';
 
 import {Colors} from '../../../constants';
 import styles from './IconButton.styles';
+import DefaultText from '../DefaultText/DefaultText';
 
-const IconButton = ({name, size, color, onPress}) => (
+const renderIcon = (name, color, size) => (
+  <Icon
+    name={name}
+    color={color ? color : Colors.accent}
+    size={size ? size : 21}
+  />
+);
+
+const renderText = (children, color, name) => (
+  <DefaultText
+    style={{
+      ...styles.text,
+      color: color ? color : Colors.accent,
+      marginLeft: name && 6,
+    }}>
+    {children}
+  </DefaultText>
+);
+
+const IconButton = ({
+  name,
+  size,
+  color,
+  backgroundColor,
+  onPress,
+  children,
+}) => (
   <Pressable
     onPress={onPress}
-    style={({pressed}) => [styles.button, pressed && styles.pressed]}>
-    <Icon
-      name={name}
-      size={size ? size : 21}
-      color={color ? color : Colors.accent}
-    />
+    style={({pressed}) => [
+      styles.button,
+      pressed && styles.pressed,
+      children && styles.hasText,
+      backgroundColor && {backgroundColor: backgroundColor},
+    ]}>
+    {name && renderIcon(name, color, size)}
+    {children && renderText(children, color, name)}
   </Pressable>
 );
 
