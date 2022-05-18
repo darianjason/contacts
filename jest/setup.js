0dec7daaa276db27e3d4aaa5871f400c1407af46
Darian Jason <darianjason@gmail.com>
2022-05-18 16:04:01 +0700

    Set up React Navigation testing configuration

---
+++ jest/setup.js
@@ -0,0 +1,21 @@
+import 'react-native-gesture-handler/jestSetup';
+
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+
+  Reanimated.default.call = () => {};
+
+  return Reanimated;
+});
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
+jest.mock('@react-navigation/native', () => {
+  return {
+    ...jest.requireActual('@react-navigation/native'),
+    useNavigation: () => ({
+      navigate: jest.fn(),
+      setOptions: jest.fn(),
+    }),
+  };
+});
