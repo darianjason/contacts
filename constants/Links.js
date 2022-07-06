bc4f801ac34366e456840107c354999b47a20f98
Darian Jason <darianjason@gmail.com>
2022-07-06 10:13:38 +0700

    Add Flow to constants

--- constants/Links.js
+++ constants/Links.js
@@ -1,3 +1,7 @@
-export default {
+// @flow
+
+const Links: {[string]: string} = {
   BASE_URL: 'https://simple-contact-crud.herokuapp.com',
 };
+
+export default Links;
--- constants/index.js
+++ constants/index.js
@@ -1,3 +1,5 @@
+// @flow
+
 export {default as Colors} from './Colors';
 export {default as Links} from './Links';
 export {INPUT_CHANGE, INPUT_BLUR, INPUT_UPDATE} from './Actions';
